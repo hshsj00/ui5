b40e291131b6b26ead6bcfefcbfffea9f76baf96
hshsj00 <hsj10320@gmail.com>
2025-03-07 16:31:10 +0900

    property.binding

---
+++ property.binding/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["sync/c25/property/binding/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ property.binding/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"syncc25/property.binding/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
