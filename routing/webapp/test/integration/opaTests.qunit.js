2ed955ede3fb973a90c073e3556d90b5ad9649d0
hshsj00 <hsj10320@gmail.com>
2025-03-14 18:16:39 +0900

    routing

---
+++ routing/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["sync/c25/routing/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ routing/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"syncc25/routing/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
